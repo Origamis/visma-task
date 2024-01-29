7a4f19f69c3c1110595c019309879c1ae91a3389
andrius-butent <andrius.antipovas@gmail.com>
2024-01-29 12:13:31 +0200

    first commit

---
+++ src/main/java/com/visma/VismaTaskApplication.java
@@ -0,0 +1,12 @@
+package com.visma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VismaTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VismaTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/visma/VismaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VismaTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

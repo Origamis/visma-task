65ebe6025d361fba11f9c2d7f8e8efee0bbe557b
andrius-butent <andrius.antipovas@gmail.com>
2024-01-29 16:04:23 +0200

    Added Transaction POJO

---
+++ src/main/java/com/visma/model/Transaction.java
@@ -0,0 +1,30 @@
+package com.visma.model;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Transaction {
+
+    @CsvBindByName(column = "transaction_id")
+    private String transactionId;
+
+    @CsvBindByName(column = "customer_id")
+    private String customerId;
+
+    @CsvBindByName(column = "item_id")
+    private String itemId;
+
+    @CsvDate(value = "yyyy-MM-dd")
+    @CsvBindByName(column = "transaction_date")
+    private Date transactionDate;
+
+    @CsvBindByName(column = "item_price")
+    private Double price;
+
+    @CsvBindByName(column = "item_quantity")
+    private Integer quantity;
+}
